57c8f7d0b817f564099c01d5c10150b9b333802c
nida.j <nida.jawre@gmail.com>
2020-11-03 00:10:51 +0530

    Added: A coin is flipped once to display head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+flip=$((RANDOM%2))
+if (( $flip == 1 ))
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
